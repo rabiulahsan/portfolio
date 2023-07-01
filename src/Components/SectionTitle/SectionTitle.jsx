d3d170ca48cda25246621c9584733338c65b2e2f
rabiulahsan <ra.rabiulahsan@gmail.com>
2023-07-01 16:11:10 +0600

    section title component create for entire project

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const SectionTitle = ({ heading }) => {
+  return (
+    <div>
+      <div className=" text-center mx-auto text-3xl uppercase  font-bold py-[2%] flex flex-col w-1/3">
+        <h3 className="">{heading}</h3>
+        <div className="divider "></div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
