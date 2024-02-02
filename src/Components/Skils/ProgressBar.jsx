5dec69d6db91fc0cc1545424995ac62d577e58bf
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-02-02 21:09:59 +0600

    single progressbar animation added

--- src/Components/Skils/ProgressBar.jsx
+++ src/Components/Skils/ProgressBar.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { motion } from "framer-motion";
+
 const ProgressBar = ({ name, learn }) => {
   console.log(learn);
   return (
@@ -6,15 +8,28 @@ const ProgressBar = ({ name, learn }) => {
       <p className="flex justify-between items-center text-slate-800 dark:text-white mb-2">
         <span>{name}</span> <span>{learn}%</span>
       </p>
-      {/* <div className="w-full bg-gray-200 rounded-full">
-        <div className={`h-2 bg-blue-400 rounded-full w-[${learn}]`}></div>
-      </div> */}
 
-      <progress
+      <div className="w-full bg-gray-200 dark:bg-slate-600 rounded-full">
+        <motion.div
+          className={`h-2 bg-blue-400 rounded-full tranfo`}
+          style={{ transformOrigin: "left" }}
+          initial={{ scaleX: 0 }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+            transition: { type: "spring", delay: 0.7, duration: 2 },
+            scaleX: `${learn}%`,
+          }}
+        ></motion.div>
+      </div>
+
+      {/* <motion.progress
         className="progress progress-info dark:bg-slate-600 w-full"
         value={learn}
         max="100"
-      ></progress>
+        initial={{ x: -100 }}
+        whileInView={{ x: 0, transition: { delay: 0.7, duration: 0.5 } }}
+      ></motion.progress> */}
     </div>
   );
 };
