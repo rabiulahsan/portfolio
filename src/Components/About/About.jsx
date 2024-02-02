cbd5b63b47fea0a9bb8d1fb9da2e442c8bd5ad70
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-02-02 19:46:47 +0600

    about section animated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import "./About.css";
 import { Link } from "react-scroll";
+import FadeAnimations from "../FadeAnimations/FadeAnimations";
 
 const About = () => {
   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
@@ -21,32 +22,48 @@ const About = () => {
       <SectionTitle heading="About Me"></SectionTitle>
       <div className="px-[10%] py-[4%] flex gap-y-8 justify-between items-center flex-col lg:flex-row lg:px-[20%]">
         <div className="lg:w-1/2">
-          <img
-            src="https://i.ibb.co/mFY4YvC/rabiul.jpg"
-            alt=""
-            className="h-[300px] rounded-full"
-          />
+          <FadeAnimations
+            direction="right"
+            once={true}
+            delay={0.3}
+            duration={0.5}
+          >
+            <img
+              src="https://i.ibb.co/mFY4YvC/rabiul.jpg"
+              alt=""
+              className="h-[300px] rounded-full"
+            />
+          </FadeAnimations>
         </div>
         <div className="w-full lg:w-1/2 lg:text-left lg:px-[4%] text-center">
-          <p className=" w-full  text-gray-600 dark:text-gray-400 mb-5 text-lg leading-8">
-            Hello! I am Rabiul Ahsan. I live in Barishal, Bangladesh. Right now,
-            i am an undergraduate in Computer Science and Engineering at UGV. I
-            am a Web Developer. I can build a website by using MERNstack
-            technology. Besides it, I learned some tools, which makes a website
-            looks more attractive and efficient. Some of my projects are given
-            in this portfolio.
-          </p>
-          <span>
-            <Link to="contact" smooth={true} duration={500} offset={-50}>
-              <button
-                onMouseMove={(e) => handleMouseMove(e)}
-                style={buttonStyles}
-                className={`button px-6 py-2 font-semibold text-lg rounded text-white inline-flex relative overflow-hidden bg-blue-400 before:bg-slate-700 dark:bg-blue-400 dark:border dark:border-blue-400  dark:before:bg-slate-900`}
-              >
-                <span className="relative z-10">Contact Me</span>
-              </button>
-            </Link>
-          </span>
+          <FadeAnimations
+            direction="left"
+            once={true}
+            delay={0.3}
+            duration={0.5}
+          >
+            <>
+              <p className=" w-full  text-gray-600 dark:text-gray-400 mb-5 text-lg leading-8">
+                Hello! I am Rabiul Ahsan. I live in Barishal, Bangladesh. Right
+                now, i am an undergraduate in Computer Science and Engineering
+                at UGV. I am a Web Developer. I can build a website by using
+                MERNstack technology. Besides it, I learned some tools, which
+                makes a website looks more attractive and efficient. Some of my
+                projects are given in this portfolio.
+              </p>
+              <span>
+                <Link to="contact" smooth={true} duration={500} offset={-50}>
+                  <button
+                    onMouseMove={(e) => handleMouseMove(e)}
+                    style={buttonStyles}
+                    className={`button px-6 py-2 font-semibold text-lg rounded text-white inline-flex relative overflow-hidden bg-blue-400 before:bg-slate-700 dark:bg-blue-400 dark:border dark:border-blue-400  dark:before:bg-slate-900`}
+                  >
+                    <span className="relative z-10">Contact Me</span>
+                  </button>
+                </Link>
+              </span>
+            </>
+          </FadeAnimations>
         </div>
       </div>
     </div>
